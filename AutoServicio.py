0bef2bc2d9ec52fb40cba1ce2161ced2762ce180
Alfred024 <21030761tcelaya.edu.mx>
2023-01-06 00:18:47 -0600

    Ctm github

--- AutoServicio.py
+++
@@ -1,8 +0,0 @@
-nombreCliente = "Manuel García";
-tipoGasolina = "E";
-litrosGasolina = 50;
-lavado = "S";
-
-row1Format = "AUTOSERVICIO LINCE";
-row2Format = "REPORTE SEMANAL DE GASOLINA Y LAVADO ";
-row3Format = "NOMBRE TIPO GAS PRECIO/lt. CANT.(lts) COSTO GAS COSTO LAV A PAGAR";
